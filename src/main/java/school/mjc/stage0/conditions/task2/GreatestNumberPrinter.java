86ea757552e6a925f18d88abe7e58c2254d163e3
Valerii Krutoshynskyi <valera9496@gmail.com>
2023-01-14 23:34:46 +0100

    Task 4/1

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if (first > second) {
+            System.out.println(first);
+        }
+        else if ( first < second){
+            System.out.println(second);
+        }
+        else {
+            System.out.println(first == second);
+        }
     }
 }
